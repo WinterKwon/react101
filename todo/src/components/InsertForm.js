fbe38534f2e666f6b837b216da439e5f8afc11a1
WinterKwon <winterkwon7017@gmail.com>
2022-06-12 10:38:05 +0900

    기본 경로 설정

---
+++ todo/src/components/InsertForm.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function InsertForm (){
+console.log('hi')
+
+}
+
+export default InsertForm;
---
+++ todo/src/components/index.js
@@ -0,0 +1,7 @@
+import InsertForm from "./InsertForm";
+import ListView from "./ListView";
+
+// export * from "./InsertForm";
+// export * from "./ListView";
+
+export {InsertForm, ListView};
